b09b2e3cdd9e04095bf91c64d72976f3d9b0bce3
DansuraitoAhiru <vqviet2007@gmail.com>
2026-09-02 12:38:42 +0700

    Add files via upload

---
+++ research_management/app/routers/auth.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, status
+from app.services.auth_service import register_service, login_service, refresh_service
+from app.db.database import get_db
+from sqlalchemy.orm import Session
+from app.schemas.auth import RegisterRequest, TokenResponse, RegisterResponse, LoginRequest, RefreshRequest
+from fastapi import Depends, Request
+from app.utils.responses import create_response
+from fastapi.security import OAuth2PasswordRequestForm
+from app.dependencies.dependency import security
+from app.models.users import User
+from app.dependencies.dependency import get_current_user
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+@router.post("/register", status_code=status.HTTP_201_CREATED, summary="Đăng ký")
+def register(request: Request, user: RegisterRequest, db: Session=Depends(get_db)):  # Depends là 1 hàm có nv tự chạy hàm truyền vào trước, lấy kết quả của nó rồi đưa kết quả vào lại parameter cho tôi
+    new_user = register_service(user, db)
+    data = RegisterResponse.model_validate(new_user)
+    return create_response(request, 201, "Đăng ký tài khoản thành công", data.model_dump(), None)
+
+
+@router.post("/login", summary="Đăng nhập")
+# def login(data: OAuth2PasswordRequestForm=Depends(), db: Session=Depends(get_db)):   # FastAPI hãy tự tạo OAuth2PasswordRequestForm và inject nó vào form_data
+def login(data: LoginRequest, db: Session=Depends(get_db)):
+    access_token, refresh_token = login_service(data, db)
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer"
+    }
+
+
+@router.post("/refresh", response_model=TokenResponse, summary="Tạo refresh Token")
+def refresh_token(refresh_token: RefreshRequest, db: Session=Depends(get_db)):
+    access_token = refresh_service(refresh_token, db)
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
---
+++ research_management/app/routers/research_project.py
@@ -0,0 +1,46 @@
+from fastapi import HTTPException, status, APIRouter, Depends, Request
+from sqlalchemy.orm import Session
+from app.schemas.project import ProjectCreate, ProjectUpdate, ProjectResponse
+from app.services.project_service import create_project_service, get_projects_service, get_project_service, update_project_service, delete_project_service
+from app.models.users import User
+from app.db.database import get_db
+from app.dependencies.dependency import get_current_user
+from app.utils.responses import create_response
+
+router = APIRouter(
+    prefix="/research-projects",
+    tags=["Project"]
+)
+
+
+# response_model: FastAPI dùng để định dạng + validate dữ liệu trả về API
+# model_validate: Pydantic dùng để chuyển một dữ liệu hoặc object thành Pydantic model - một class dùng để định nghĩa và kiểm tra (validate) dữ liệu trong Python
+@router.post("", status_code=status.HTTP_201_CREATED, summary="Tạo đề tài nghiên cứu")
+def create_project(request: Request, project: ProjectCreate, owner: User = Depends(get_current_user), db: Session=Depends(get_db)):
+    new_project = create_project_service(project, owner, db)
+    data = ProjectResponse.model_validate(new_project)
+    return create_response(request, 201, "Thêm đề tài nghiên mới thành công", data.model_dump(), None)
+
+
+@router.get("", response_model=list[ProjectResponse], summary="Lấy danh sách đề tài")
+def get_projects(user: User = Depends(get_current_user), db: Session=Depends(get_db), keyword: str | None = None):
+    return get_projects_service(user, db, keyword)
+
+
+@router.get("/{id}", summary="Lấy chi tiết đề tài")
+def get_project(id: int, user: User=Depends(get_current_user), db: Session=Depends(get_db)):
+    return get_project_service(id, user, db)
+
+
+@router.patch("/{id}", summary="Cập nhật đề tài")
+def update_project(request: Request, data: ProjectUpdate, id: int, user: User=Depends(get_current_user), db: Session=Depends(get_db)):
+    project = update_project_service(data, id, user, db)
+    data = ProjectResponse.model_validate(project)
+    return create_response(request, 200, "Cập nhật đề tài nghiên cứu thành công", data.model_dump(), None)
+
+
+@router.delete("/{id}", summary="Xóa đề tài")
+def delete_project(request: Request, id: int, user: User=Depends(get_current_user), db: Session=Depends(get_db)):
+    deleted_project = delete_project_service(id, user, db)
+    data = ProjectResponse.model_validate(deleted_project)
+    return create_response(request, 200, "Xóa đề tài nghiên cứu thành công", data.model_dump(), None)
